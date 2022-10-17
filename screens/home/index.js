3a4ff4980339dd8f30a4e880c72e894c901f3b32
jappabr <guilherme.dion@gmail.com>
2022-10-16 22:53:54 -0300

    Create index.js

---
+++ screens/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View, FlatList } from 'react-native';
+import {styles} from './styles'
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  );
+}
+
