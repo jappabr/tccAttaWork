bb485aa92e1f4d298d6a32f7e15a4eac0b1dbc10
jappabr <guilherme.dion@gmail.com>
2022-10-16 22:53:22 -0300

    Create index.js

---
+++ components/input/index.js
@@ -0,0 +1,29 @@
+import {View, TextInput, Dimensions} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { styles } from './styles';
+
+const {width, height} = Dimensions.get("screen");
+
+const Input = ({icon, size, placeholder, onChangeText, secureTextEntry})=>{
+  return(
+    <View style={styles.container}>
+      <View style={styles.icon}>
+        <MaterialCommunityIcons
+          name={icon}
+          style={{color:"#0066cc"}}
+          size={25}
+        />
+      </View>
+      <View style={styles.input}>
+        <TextInput
+          style={{fontSize: 16}}
+          placeholder={placeholder}
+          placeholderTextColor="gray"
+          onChangeText={onChangeText}
+          secureTextEntry={secureTextEntry}
+        />
+      </View>
+    </View>
+  )
+}
+export default Input;
