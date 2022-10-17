6bbc75c815eec7d8531d2be2e4d6188c9e006598
jappabr <guilherme.dion@gmail.com>
2022-10-16 22:54:06 -0300

    Create styles.js

---
+++ screens/login/styles.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+  background:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  logoDiv:{
+    justifyContent: 'center',
+    alignItems:'center',
+    height: '40%'
+  },
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%'
+  },
+  input:{
+    backgroundColor: '#fff',
+    width: '90%',
+    marginBottom: 15,
+    color:'#222',
+    fontSize:17,
+    borderRadius: 7,
+    padding:10,
+  },
+  btnLogin:{
+    flexDirection:'row',
+    backgroundColor:'#0066cc',
+    width:'100%',
+    alignItems: 'center',
+    justifyContent:'center',
+    borderRadius: 15,
+    marginVertical: 10,
+    height: '6%'
+  },
+  txtInput:{
+    color: '#fff',
+    fontSize: 20
+  },
+  logo:{
+    width:170,
+    height:170,
+    resizeMode: 'stretch'
+  },
+  titulo:{
+    fontSize: 20,
+    fontFamily: 'OpenSans',
+    color: '#fff',
+  },
+  btnCadastro:{
+    color:'#0066cc',
+    padding:10,
+    fontSize: 15,
+  }
+});
