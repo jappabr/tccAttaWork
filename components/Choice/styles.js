ddc248206a250bda518987fb0bc4522848b59305
jappabr <guilherme.dion@gmail.com>
2022-10-16 22:53:03 -0300

    Create styles.js

---
+++ components/Choice/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        borderWidth: 7,
+        paddingHorizontal: 15,
+        borderRadius: 15,
+        backgroundColor: '#0003'
+    },
+    text: {
+        fontSize: 48,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        letterSpacing: 4
+    }
+});
