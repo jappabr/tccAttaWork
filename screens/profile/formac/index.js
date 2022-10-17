0e7026baaf5d87cdedae5b77388e03e7d2ba0d35
jappabr <guilherme.dion@gmail.com>
2022-10-16 22:54:25 -0300

    Create index.js

---
+++ screens/profile/formac/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {styles} from './styles'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  );
+}
