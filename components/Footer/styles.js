e2c4a6d1a4069df3f6886e2db0f3377d1e8fabd5
jappabr <guilherme.dion@gmail.com>
2022-10-16 22:53:15 -0300

    Create styles.js

---
+++ components/Footer/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 15,
+        width: 170,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        zIndex: -1
+    }
+});
