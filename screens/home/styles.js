bf70ea066f6d04af983f06bd5b033c9d041cf957
jappabr <guilherme.dion@gmail.com>
2022-10-16 22:53:58 -0300

    Create styles.js

---
+++ screens/home/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexGrow: 1
+  },
+  list: {
+    flex: 1,
+    flexGrow: 1,
+    paddingVertical: 8
+  }
+});
