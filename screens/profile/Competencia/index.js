2189cf98cdfeb237c9e9ec07f47be2a6afb0b572
jappabr <guilherme.dion@gmail.com>
2022-10-20 20:16:15 -0300

    Update index.js

--- screens/profile/Competencia/index.js
+++ screens/profile/Competencia/index.js
@@ -1,48 +1,129 @@
-import { View, Text, KeyboardAvoidingView, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
-import {useNavigation} from "@react-navigation/native"
-import {styles} from "./styles"
+import React, { useState, useEffect } from "react";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
-const CadastroScreen = () => {
-  const navigation=useNavigation();
+import {
+  Keyboard,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  AsyncStorage
+} from "react-native";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList
+} from "react-native";
+
+import {styles} from './styles';
+
+export default function App() {
+  const [form, setForm] = useState([]);
+  const [newForm, setNewForm] = useState("");
+
+  async function addForm() {
+    const search = form.filter(form => form === newForm);
+
+    if (search.length !== 0) {
+      Alert.alert("Atenção", "Nome da formação repetido!");
+      return;
+    }
+
+    setForm([...form, newForm]);
+    setNewForm("");
+
+    Keyboard.dismiss();
+  }
+
+  async function removeForm(item) {
+    Alert.alert(
+      "Deletar formação",
+      "Tem certeza que deseja remover esta formação?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            return;
+          },
+          style: "cancel"
+        },
+        {
+          text: "OK",
+          onPress: () => setForm(form.filter(forms => forms !== item))
+        }
+      ],
+      { cancelable: false }
+    );
+  }
+
+  useEffect(() => {
+    async function carregaDados() {
+      const form = await AsyncStorage.getItem("form");
+
+      if (form) {
+        setForm(JSON.parse(form));
+      }
+    }
+    carregaDados();
+  }, []);
+
+  useEffect(() => {
+    async function salvaDados() {
+      AsyncStorage.setItem("form", JSON.stringify(form));
+    }
+    salvaDados();
+  }, [form]);
+
   return (
-    <KeyboardAvoidingView style={styles.background}>
-    <View style={styles.logoDiv}>
-      <Image
-        style={styles.logo}
-        source={require("../../../src/logo.png")}
-      />
-      <Text style={styles.titulo}>AttaWork</Text>  
-    </View>
-    <View style={styles.container}>
-    <TextInput
-        style={styles.input} 
-        placeholder='Nome'
-        autoCorrect={false}
-        onChangeText={()=>{}}
-        />
-    <TextInput
-        style={styles.input} 
-        placeholder='Email'
-        autoCorrect={false}
-        onChangeText={()=>{}}
-        />
-    <TextInput
-        style={styles.input}
-        placeholder='Senha'
-        autoCorrect={false}
-        secureTextEntry="true"
-        onChangeText={()=>{}}
-        />
-    <TouchableOpacity
-     style={styles.btnLogin}
-     onPress={()=> 
-        navigation.navigate("Cadastro2")}
-     >
-        <Text style={styles.txtInput}>Alterar</Text>
-    </TouchableOpacity>
-    </View>
-    </KeyboardAvoidingView>
-  )
+    <>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={0}
+        behavior="padding"
+        style={{ flex: 1 }}
+        enabled={Platform.OS === "ios"}
+      >
+        <View style={styles.container}>
+          <View style={styles.Body}>
+            <FlatList
+              data={form}
+              keyExtractor={item => item.toString()}
+              showsVerticalScrollIndicator={false}
+              style={styles.FlatList}
+              renderItem={({ item }) => (
+                <View style={styles.ContainerView}>
+                  <Text style={styles.Texto}>{item}</Text>
+                  <TouchableOpacity onPress={() => removeForm(item)}>
+                    <MaterialIcons
+                      name="delete-forever"
+                      size={25}
+                      color="#f64c75"
+                    />
+                  </TouchableOpacity>
+                </View>
+              )}
+            />
+          </View>
+
+          <View style={styles.Form}>
+            <TextInput
+              style={styles.Input}
+              placeholderTextColor="gray"
+              autoCorrect={true}
+              value={newForm}
+              placeholder="Adicione sua formação"
+              maxLength={50}
+              onChangeText={text => setNewForm(text)}
+            />
+            <TouchableOpacity style={styles.Button} onPress={() => addForm()}>
+              <Ionicons name="ios-add" size={20} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </>
+  );
 }
-export default CadastroScreen
+  
