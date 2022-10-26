d6a3432ba313894ba2201e92bbbba39c4a6d4325
jappabr <guilherme.dion@gmail.com>
2022-10-26 19:44:15 -0300

    Update index.js

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,11 +1,114 @@
-import React from 'react';
-import {Text, View, FlatList } from 'react-native';
-import {styles} from './styles'
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text></Text>
-    </View>
-  );
+import React from "react";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+posts = [
+    {
+        id: "1",
+        title: "USP",
+        text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        timestamp: 1569109273726,
+        image: require("../../assets/icon.png")
+    }
+];
+
+export default class HomeScreen extends React.Component {
+    renderPost = post => {
+        return (
+            <View style={styles.feedItem}>
+                <Image source={posts.image} style={styles.avatar} />
+                <View style={{ flex: 1 }}>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                        <View>
+                            <Text style={styles.name}>Novidades - {post.title}</Text>
+                        </View>
+
+                    </View>
+                    <Text style={styles.post}>{post.text}</Text>
+                    <Image source={post.image} style={styles.postImage} resizeMode="cover" />
+                </View>
+            </View>
+        );
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.headerTitle}>Feed</Text>
+                </View>
+
+                <FlatList
+                    style={styles.feed}
+                    data={posts}
+                    renderItem={({ item }) => this.renderPost(item)}
+                    keyExtractor={item => item.id}
+                    showsVerticalScrollIndicator={false}
+                ></FlatList>
+            </View>
+        );
+    }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#EBECF4"
+    },
+    header: {
+        paddingTop: 64,
+        paddingBottom: 16,
+        backgroundColor: "#FFF",
+        alignItems: "center",
+        justifyContent: "center",
+        borderBottomWidth: 1,
+        borderBottomColor: "#EBECF4",
+        shadowColor: "#454D65",
+        shadowOffset: { height: 5 },
+        shadowRadius: 15,
+        shadowOpacity: 0.2,
+        zIndex: 10
+    },
+    headerTitle: {
+        fontSize: 20,
+        fontWeight: "500"
+    },
+    feed: {
+        marginHorizontal: 16
+    },
+    feedItem: {
+        backgroundColor: "#FFF",
+        borderRadius: 5,
+        padding: 8,
+        flexDirection: "row",
+        marginVertical: 8
+    },
+    avatar: {
+        width: 36,
+        height: 36,
+        borderRadius: 18,
+        marginRight: 16
+    },
+    name: {
+        fontSize: 15,
+        fontWeight: "500",
+        color: "#0066cc"
+    },
+    timestamp: {
+        fontSize: 11,
+        color: "#C4C6CE",
+        marginTop: 4
+    },
+    post: {
+        marginTop: 16,
+        fontSize: 14,
+        color: "#838899"
+    },
+    postImage: {
+        width: undefined,
+        height: 150,
+        borderRadius: 5,
+        marginVertical: 16
+    }
+});
