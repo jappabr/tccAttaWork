1b3cc377508b422ba43c772ae72e46618bc60db4
jappabr <guilherme.dion@gmail.com>
2022-11-10 20:30:04 -0300

    create search

--- screens/search/index.js
+++ screens/search/index.js
@@ -1,27 +1,57 @@
-import React, {useEffect, useState} from 'react';
-import { ScrollView, TextInput, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TextInput, View, FlatList, TouchableOpacity, Text, Image } from 'react-native';
 import API from '../../utils/API'
-import {getUser} from '../../utils/user';
+import { getUser } from '../../utils/user';
 import { styles } from './styles';
-
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Empty } from '../../components/Empty';
 export function Search() {
+  const [search, setSearch] = useState();
   const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch(API + 'user')
+  function pesquisar() {
+    fetch(API + 'search/?q=' + search)
       .then(response => response.json())
       .then(data => {
         setData(data)
+        console.log(data)
       });
-  }, []);
+  }
   return (
-    <View style={styles.container}>
-      <TextInput placeholder='pesquisar'/>
 
-      <ScrollView>
-        {data.map((data, index)=>(
-          <Text key={index}>{data.id}</Text>
-        ))}
-      </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.input}>
+        <TextInput
+          style={{ fontSize: 16 }}
+          placeholder={"Pesquisar o nome do usuário"}
+          placeholderTextColor="gray"
+          onChangeText={(search) => setSearch(search)}
+        />
+        <TouchableOpacity style={styles.btnPesquisar} onPress={pesquisar}>
+          <Icon name="magnify" color="#204ac8" size={25} />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+        style={styles.FlatList}
+        ListEmptyComponent={({ item }) => (
+          <View>
+            <Empty text="Sem Dados"></Empty>
+          </View>
+        )}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.cardPerfil} onPress={()=>pesquisar()}>
+            <View style={styles.card}>
+             <Image style={{width: 50, height: 50, borderRadius: 15, marginTop: 10}}source={{uri: item.profilePic}}/>
+              <Text style={{color: "#0066cc", fontSize: 15, fontStyle: "italic", fontWeight:"bold"}}>{item.nome}</Text>
+              <Text>{item.cidade}</Text>
+              <Text style={{marginBottom: 10}}>{item.email}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      >
+      </FlatList>
     </View>
   );
 }
--- screens/search/styles.js
+++ screens/search/styles.js
@@ -1,7 +1,34 @@
 import {StyleSheet} from 'react-native'
 export const styles = StyleSheet.create({
   container: {
+    paddingTop: 29,
+    backgroundColor: '#fff',
+    flexDirection: 'column',
     flex: 1,
-    backgroundColor: '#fff'
+    justifyContent: 'center'
+  },
+  input:{
+    flexDirection: 'row',
+    maxHeight: 54,
+    paddingLeft: 55
+  },
+  btnPesquisar:{
+    paddingLeft: 10,
+    color: "#fff",
+    backgroundColor: "#fff",
+    textAlign: "center",
+    height: 59,
+    width: "25%",
+    justifyContent: 'center'
+  },
+  cardPerfil:{
+    flex: 1,
+  },
+  card:{
+    borderColor:"gray", 
+    borderWidth: 1, 
+    borderRadius:10, 
+    margin: 10,
+    alignItems: 'center'
   }
 })
